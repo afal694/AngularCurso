9309e179a3db22d19b4fb1747bf58c807e07e0e1
Andres Ariza <and.694@gmail.com>
2020-07-16 01:27:17 -0500

    Validators personalizados y parametrizables

--- angular-pro1/src/app/form-destino/form-destino.component.ts
+++ angular-pro1/src/app/form-destino/form-destino.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder, Validators, ValidatorFn } from '@angular/forms';
 import { DestinoViaje } from './../models/destino-viaje.model';
 
 @Component({
@@ -11,11 +11,17 @@ export class FormDestinoComponent implements OnInit {
   
   @Output() onItemAdded: EventEmitter<DestinoViaje>;
   fg: FormGroup;
+
+  minLong = 5;
    
   constructor(private fb: FormBuilder) {
     this.onItemAdded = new EventEmitter(); 
     this.fg = this.fb.group({
-      nombre: ['',Validators.required],
+      nombre: ['',Validators.compose([
+        Validators.required,
+        this.Nombrevalidator,
+        this.NombreValidatorParametrizable(this.minLong)
+      ])],
       url: ['', Validators.required]
     });
     
@@ -32,5 +38,24 @@ export class FormDestinoComponent implements OnInit {
     this.onItemAdded.emit(new DestinoViaje(nombre,url));
     return false;
   }
+
+  Nombrevalidator (control: FormControl): { [s: string]: boolean } {
+    const l = control.value.toString().trim().length;
+    if ( l > 0 && l < 3 ){
+      return { minNombre: true };
+    }
+    return null;
+  }
+
+  NombreValidatorParametrizable( minLong: number ): ValidatorFn {
+    return (control: FormControl): { [s: string]: boolean } | null => {
+      const l = control.value.toString().trim().length;
+      if ( l > 0 && l < minLong ){
+        return { minLongNombre: true };
+      }
+      return null
+    }
+  }
+
 
 }
